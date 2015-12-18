4d184039ac78825a224314187b929f529c578153
Denis Manshilin <evodes.mdv@gmail.com>
2015-12-18 15:02:20 +0200

    template-css ver.0.1

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    concatCSS = require('gulp-concat-css'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+	imagemin = require('gulp-imagemin');
+
+// Оптимизация и подготовка CSS
+
+gulp.task('styles', function () {
+    gulp.src('css/sources/all-style.css')
+        .pipe(concatCSS('style.css'))
+        .pipe(autoprefixer())
+        .pipe(minifyCSS())
+        .pipe(rename('style.min.css')) 
+        .pipe(gulp.dest('css'))
+        .pipe(notify('Task done!'));
+});
+
+// Отслеживание изменений CSS
+
+gulp.task('watch', function () {
+    gulp.watch('css/sources/*.css', ['default'])
+});
+
+// Сжатие изображений
+
+gulp.task('imgoptim', function () {
+    gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
